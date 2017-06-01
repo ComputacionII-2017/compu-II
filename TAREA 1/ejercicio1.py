be915c34eba44ad1938adbea08cae1cab49bf996
Edgardo <emoron@gmail.com>
2017-05-31 19:43:00 -0500

    Update ejercicio1.py

--- TAREA 1/ejercicio1.py
+++ TAREA 1/ejercicio1.py
@@ -1,5 +1,8 @@
 #NIEVES MORA MARTHA ELENA
 M= [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]]
+'''
+Para la entrada es necesario tener un input raw o que pueda leer el archivo 
+'''
 x=0
 for i in  range(0,4):
     for y in  range (0,4):
